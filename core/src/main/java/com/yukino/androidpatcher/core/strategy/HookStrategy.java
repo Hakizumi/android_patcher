5d7ded1f066e2fcd4810abcf858a8c25fdd0118d
Hakizumi <Hakizumi@outlook.com>
2026-07-14 16:55:42 +0800

    feat: Add the `HookStrategy`

---
+++ core/src/main/java/com/yukino/androidpatcher/core/strategy/HookStrategy.java
@@ -0,0 +1,13 @@
+package com.yukino.androidpatcher.core.strategy;
+
+/**
+ * The hook strategy decider interface.
+ * Responsible for deciding a hooking action's {@code params}
+ * (Which method,which field or other profile) by the current hooking app's information.
+ * 
+ * @param <T> The provided hooking profile,a {@code POJO}.
+ */
+@FunctionalInterface
+public interface HookStrategy<T> {
+    T provideProfile(String version);
+}

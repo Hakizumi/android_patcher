453751557e4187579bb6789f1e18763be98881a1
Hakizumi <Hakizumi@outlook.com>
2026-07-15 13:50:17 +0800

    feat: Add the `UserPlusProfile`

---
+++ duolingo/src/main/java/com/yukino/androidpatcher/duolingo/hooks/profile/UserPlusProfile.java
@@ -0,0 +1,12 @@
+package com.yukino.androidpatcher.duolingo.hooks.profile;
+
+/**
+ * The user-plus-subscription hooking profile.
+ */
+public class UserPlusProfile {
+    /** User class full name */
+    public String userClass = "com.duolingo.data.user.User";
+
+    /** {@code hasPlus} attribute field name */
+    public String hasPlusField;
+}
